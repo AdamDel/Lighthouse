778af97c5b664accd3c1ea5f51052871e51829b4
AdamDel <adam.devbootcamp@gmail.com>
2015-12-16 14:45:32 -0700

    Adding code

--- Recipe/script.js
+++ Recipe/script.js
@@ -1,34 +1,33 @@
 $(document).ready(function() {
-  console.log("Script included!");
-
-	$( "input[type=checkbox]" ).on( "click", function(){
-
-		$(this).parent().toggleClass("checked");
+ 
+	  $( "input[type=checkbox]" ).on( "click", function(){
+		    $(this).parent().toggleClass("checked");
 			
-	});
+	  });
+
   	$(".camera").mouseover(function() {
-    $(".camera").css({ width: '34px', height: 'auto' });
-});
+        $(".camera").css({ width: '34px', height: 'auto' });
+    });
   
-$(".camera").mouseleave(function() {
-    $(".camera").css({ width: '30px', height: 'auto' });
-});
+    $(".camera").mouseleave(function() {
+       $(".camera").css({ width: '30px', height: 'auto' });
+    });
 
   
   	$(".camera").click(function(){
 
-       var el = $(".camclick");
-  if (el.text() == el.data("text-swap")) {
-    el.text(el.data("text-original"));
-  } else {
-    el.data("text-original", el.text());
-    el.text(el.data("text-swap"));
-  }
+      var el = $(".camclick");
+      if (el.text() == el.data("text-swap")) {
+          el.text(el.data("text-original"));
+        } else {
+          el.data("text-original", el.text());
+          el.text(el.data("text-swap"));
+        }
 
-  		$(".instruction").fadeToggle("slow",function(){
+    $(".instruction").fadeToggle("slow",function(){
 
 
-  		});
+    });
 
   });
 
